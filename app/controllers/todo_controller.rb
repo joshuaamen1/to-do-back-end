b8b0fda010ad2648069421c464a4918a5b8eff69
Joshua <jhenry19@sjnd.org>
2017-06-18 20:35:23 +0000

    datebase work almost complete

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,29 +1,32 @@
 class TodoController < ApplicationController
     def index
     end
-    
     def show
-    todo_id = params[:id]
+        @todo = Todo.find_by_id(params[:id])
+    end
+    #def show
+    #todo_id = params[:id]
    # The ... signifies that there is more code in this section, but is not shown in the example for the sake of brevity
-    end
+    #end
   
-    def show
-    todo_id = '1'
+    #def show
+    #todo_id = '1'
   
-    if todo_id == '1' # Remember, the `==` conditional operator is used to check if two things are equal to each other.
-        @todo_description = " feed the cats"
-    end
+    #if todo_id == '1' # Remember, the `==` conditional operator is used to check if two things are equal to each other.
+        # @todo.description = " feed the cats"
+    #end
   
-    end
-    def show
-    todo_id = '2'
+    #end
+    #def show
+    #todo_id = '2'
+    #@todo = Todo.find_by_id(params['id'])
+    #if todo_id == '1'
+        # @todo.description = "feed the cats "
+  
 
-    if todo_id == '1'
-        @todo_description = "feed the cats "
-  
-
-    elsif todo_id == '2'
-        @todo_description = "go for a run"
-    end
-    end
+    #elsif todo_id == '2'
+        # @todo.description = "go for a run"
+    #end
+    #end
+    
 end
